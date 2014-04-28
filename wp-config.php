bb5d088b7e3fa4e15dec3c03f45dde0b054a935d
Sergey Ustinov <sergejustinov@MacBook-Pro-Sergej-Ustinov.local>
2014-04-28 23:01:22 +0400

    вп конфиг

--- wp-config.php
+++ wp-config.php
@@ -16,17 +16,14 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'black_one');
+define('DB_NAME', 'u356887_table');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
 
-define('DB_PASSWORD', '');
-
 define('DB_PASSWORD', 'root');
-
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
